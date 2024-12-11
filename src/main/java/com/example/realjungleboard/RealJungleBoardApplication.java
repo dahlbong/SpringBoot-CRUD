7331f5dfb538f1f8038cfb90c12e9ffec1852ade
dahyun <halover111@gmail.com>
2024-12-11 19:42:58 +0900

    initial commit

---
+++ src/main/java/com/example/realjungleboard/RealJungleBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.realjungleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealJungleBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealJungleBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/realjungleboard/RealJungleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realjungleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealJungleBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
